3162857edd6107e3d00fcf4272d8b39dc794181e
马丹 <dan@madandeMacBook-Air.local>
2019-01-28 23:07:44 +0800

    Initial commit.

---
+++ EdgeConnect-CoreML/EdgeConnect-CoreML/UIImage+Resize.swift
@@ -0,0 +1,29 @@
+//
+//  UIImage+Resize.swift
+//
+
+import UIKit
+
+extension UIImage {
+    // Resizeing using CoreGraphics
+    func resize(to size:CGSize) -> UIImage? {
+        let cgImage = self.cgImage!
+
+        let destWidth = Int(size.width)
+        let destHeight = Int(size.height)
+        let bitsPerComponent = 8
+        let bytesPerPixel = cgImage.bitsPerPixel / bitsPerComponent
+        let destBytesPerRow = destWidth * bytesPerPixel
+
+        let context = CGContext(data: nil,
+                                width: destWidth,
+                                height: destHeight,
+                                bitsPerComponent: bitsPerComponent,
+                                bytesPerRow: destBytesPerRow,
+                                space: cgImage.colorSpace!,
+                                bitmapInfo: cgImage.bitmapInfo.rawValue)!
+        context.interpolationQuality = .high
+        context.draw(cgImage, in: CGRect(origin: CGPoint.zero, size: size))
+        return context.makeImage().flatMap { UIImage(cgImage: $0) }
+    }
+}
---
+++ EdgeConnect-CoreML/EdgeConnect-CoreML/ViewController.swift
@@ -0,0 +1,320 @@
+//
+//  ViewController.swift
+//  EdgeConnect-CoreML
+//
+//  Created by 马丹 on 2019/1/27.
+//  Copyright © 2019 马丹. All rights reserved.
+//
+
+import UIKit
+import CoreML
+
+class ViewController: UIViewController {
+    typealias FilteringCompletion = ((UIImage?, Error?) -> ())
+
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var loader: UIActivityIndicatorView!
+    @IBOutlet private var buttonHolderView: UIView!
+    @IBOutlet private var applyButton: UIButton!
+    @IBOutlet private var loaderWidthConstraint: NSLayoutConstraint!
+
+    var imagePicker = UIImagePickerController()
+    var isProcessing : Bool = false {
+        didSet {
+            self.applyButton.isEnabled = !isProcessing
+            self.isProcessing ? self.loader.startAnimating() : self.loader.stopAnimating()
+            self.loaderWidthConstraint.constant = self.isProcessing ? 20.0 : 0.0
+            UIView.animate(withDuration: 0.3) {
+                self.isProcessing
+                    ? self.applyButton.setTitle("Processing...", for: .normal)
+                    : self.applyButton.setTitle("Apply EdgeConnect", for: .normal)
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+
+    //MARK:- Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.isProcessing = false
+        self.applyButton.superview!.layer.cornerRadius = 4
+    }
+
+    //MARK: - Utils
+    func showError(_ error: Error) {
+        self.buttonHolderView.backgroundColor = UIColor(red: 220/255, green: 50/255, blue: 50/255, alpha: 1)
+        self.applyButton.setTitle(error.localizedDescription, for: .normal)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
+            self.applyButton.setTitle("Apply Style", for: .normal)
+            self.buttonHolderView.backgroundColor = UIColor(red: 5/255, green: 122/255, blue: 255/255, alpha: 1)
+        }
+    }
+
+    //MARK:- CoreML
+
+    func testMLMultiArray(pixelBuffer: CVPixelBuffer, data: MLMultiArray, height: Int, width: Int) {
+        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+        let buffer = baseAddress!.assumingMemoryBound(to: UInt8.self)
+
+        var ptrData = UnsafeMutablePointer<Float>(OpaquePointer(data.dataPointer))
+        ptrData = ptrData.advanced(by: 0)
+
+        let cStride = width * height
+
+        for y in 0..<height {
+            for x in 0..<width {
+                ptrData[y*width + x + cStride * 0] = (Float)(buffer[y*bytesPerRow+x*4+1])
+                ptrData[y*width + x + cStride * 1] = (Float)(buffer[y*bytesPerRow+x*4+2])
+                ptrData[y*width + x + cStride * 2] = (Float)(buffer[y*bytesPerRow+x*4+3])
+            }
+        }
+    }
+
+    func getGrayImage(pixelBuffer: CVPixelBuffer, data: MLMultiArray, height: Int, width: Int) {
+        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+        let buffer = baseAddress!.assumingMemoryBound(to: UInt8.self)
+
+        var ptrData = UnsafeMutablePointer<Float>(OpaquePointer(data.dataPointer))
+        ptrData = ptrData.advanced(by: 0)
+
+        for y in 0..<height {
+            for x in 0..<width {
+                let index = y*bytesPerRow+x*4
+                ptrData[y*width + x] = ((Float)(buffer[index+1]) * 0.299 + (Float)(buffer[index+2]) * 0.587 + (Float)(buffer[index+3]) * 0.114) / 255.0
+            }
+        }
+    }
+
+    func calcNorm(r: Float, g: Float, b: Float) -> Float {
+        return ((r-255.0)*(r-255.0) + (g-255.0)*(g-255.0) + (b-255.0)*(b-255.0)).squareRoot()
+    }
+
+    func getMask(pixelBuffer: CVPixelBuffer, data: MLMultiArray, height: Int, width: Int) {
+        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+        let buffer = baseAddress!.assumingMemoryBound(to: UInt8.self)
+
+        var ptrData = UnsafeMutablePointer<Float>(OpaquePointer(data.dataPointer))
+        ptrData = ptrData.advanced(by: 0)
+
+        for y in 0..<height {
+            for x in 0..<width {
+                let index = y*bytesPerRow+x*4
+                if calcNorm(r: (Float)(buffer[index+1]), g: (Float)(buffer[index+2]), b: (Float)(buffer[index+3])) > 8.66 {
+                    ptrData[y*width + x] = 0
+                } else {
+                    ptrData[y*width + x] = 1
+                }
+            }
+        }
+    }
+
+    func prepareEdgeInput(gray: MLMultiArray, edge: MLMultiArray, mask: MLMultiArray, input: MLMultiArray, height: Int, width: Int) {
+        var ptrGray = UnsafeMutablePointer<Float>(OpaquePointer(gray.dataPointer))
+        ptrGray = ptrGray.advanced(by: 0)
+        var ptrEdge = UnsafeMutablePointer<Float>(OpaquePointer(edge.dataPointer))
+        ptrEdge = ptrEdge.advanced(by: 0)
+        var ptrMask = UnsafeMutablePointer<Float>(OpaquePointer(mask.dataPointer))
+        ptrMask = ptrMask.advanced(by: 0)
+        var ptrInput = UnsafeMutablePointer<Float>(OpaquePointer(input.dataPointer))
+        ptrInput = ptrInput.advanced(by: 0)
+
+        let cStride = width * height
+
+        for y in 0..<height {
+            for x in 0..<width {
+                if ptrMask[y*width + x] != 0 {
+                    ptrInput[y*width + x + cStride * 0] = ptrMask[y*width + x]
+                } else {
+                    ptrInput[y*width + x + cStride * 0] = ptrGray[y*width + x]
+                }
+                ptrInput[y*width + x + cStride * 1] = 0
+                ptrInput[y*width + x + cStride * 2] = ptrMask[y*width + x]
+            }
+        }
+    }
+
+    func prepareInpaintingInput(image: CVPixelBuffer, mask: MLMultiArray, edge: MLMultiArray, input: MLMultiArray, height: Int, width: Int) {
+        CVPixelBufferLockBaseAddress(image, CVPixelBufferLockFlags(rawValue: 0))
+        let baseAddress = CVPixelBufferGetBaseAddress(image)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(image)
+        let buffer = baseAddress!.assumingMemoryBound(to: UInt8.self)
+
+        var ptrMask = UnsafeMutablePointer<Float>(OpaquePointer(mask.dataPointer))
+        ptrMask = ptrMask.advanced(by: 0)
+        var ptrEdge = UnsafeMutablePointer<Float>(OpaquePointer(edge.dataPointer))
+        ptrEdge = ptrEdge.advanced(by: 0)
+        var ptrInput = UnsafeMutablePointer<Float>(OpaquePointer(input.dataPointer))
+        ptrInput = ptrInput.advanced(by: 0)
+
+        let cStride = width * height
+
+        for y in 0..<height {
+            for x in 0..<width {
+                let index = y*bytesPerRow+x*4
+                if ptrMask[y*width + x] != 0 {
+                    ptrInput[y*width + x + cStride * 0] = ptrMask[y*width + x]
+                    ptrInput[y*width + x + cStride * 1] = ptrMask[y*width + x]
+                    ptrInput[y*width + x + cStride * 2] = ptrMask[y*width + x]
+                } else {
+                    ptrInput[y*width + x + cStride * 0] = (Float)(buffer[index+1]) / 255.0
+                    ptrInput[y*width + x + cStride * 1] = (Float)(buffer[index+2]) / 255.0
+                    ptrInput[y*width + x + cStride * 2] = (Float)(buffer[index+3]) / 255.0
+                }
+                ptrInput[y*width + x + cStride * 3] = ptrEdge[y*width + x]
+            }
+        }
+    }
+
+    func process(input: UIImage, completion: @escaping FilteringCompletion) {
+        let startTime = CFAbsoluteTimeGetCurrent()
+
+        // Initialize the EdgeConnect model
+        let model_edge = edge()
+        let model_inpainting = inpainting()
+
+        // Next steps are pretty heavy, better process them on another thread
+        DispatchQueue.global().async {
+            // 1 - Resize our input image
+            guard let inputImage = input.resize(to: CGSize(width: 320, height: 320)) else {
+                completion(nil, EdgeConnectError.resizeError)
+                return
+            }
+
+            // 2 - Edge Model
+            guard let cvBufferInput = inputImage.pixelBuffer() else {
+                completion(nil, EdgeConnectError.pixelBufferError)
+                return
+            }
+
+            guard let mlGray = try? MLMultiArray(shape: [1, 320, 320], dataType: MLMultiArrayDataType.float32) else {
+                completion(nil, EdgeConnectError.allocError)
+                return
+            }
+
+            self.getGrayImage(pixelBuffer: cvBufferInput, data: mlGray, height: 320, width: 320)
+            //let image = mlGray.image(min: 0, max: 1, axes: (0, 1, 2))
+
+            guard let mlMask = try? MLMultiArray(shape: [1, 320, 320], dataType: MLMultiArrayDataType.float32) else {
+                completion(nil, EdgeConnectError.allocError)
+                return
+            }
+
+            self.getMask(pixelBuffer: cvBufferInput, data: mlMask, height: 320, width: 320)
+            //let image = mlMask.image(min: 0, max: 1, axes: (0, 1, 2))
+
+            guard let mlInputEdge = try? MLMultiArray(shape: [3, 320, 320], dataType: MLMultiArrayDataType.float32) else {
+                completion(nil, EdgeConnectError.allocError)
+                return
+            }
+
+            self.prepareEdgeInput(gray: mlGray, edge: mlMask, mask: mlMask, input: mlInputEdge, height: 320, width: 320)
+
+            guard let inputEdge = try? edgeInput(input_1: mlInputEdge) else {
+                completion(nil, EdgeConnectError.allocError)
+                return
+            }
+
+            guard let edgeOutput = try? model_edge.prediction(input: inputEdge) else {
+                completion(nil, EdgeConnectError.predictionError)
+                return
+            }
+            //let image = edgeOutput._153.image(min: 0, max: 1, axes: (0, 1, 2))
+
+            // 3 - InPainting model
+            guard let mlInputInpainting = try? MLMultiArray(shape: [4, 320, 320], dataType: MLMultiArrayDataType.float32) else {
+                completion(nil, EdgeConnectError.allocError)
+                return
+            }
+
+            self.prepareInpaintingInput(image: cvBufferInput, mask: mlMask, edge: edgeOutput._153, input: mlInputInpainting, height: 320, width: 320)
+
+            guard let inputInpainting = try? inpaintingInput(input_1: mlInputInpainting) else {
+                completion(nil, EdgeConnectError.allocError)
+                return
+            }
+
+            guard let inpaintingOutput = try? model_inpainting.prediction(input: inputInpainting) else {
+                completion(nil, EdgeConnectError.predictionError)
+                return
+            }
+            let image = inpaintingOutput._173.image(min: 0, max: 1, axes: (0, 1, 2))
+
+            // 4 - Hand result to main thread
+            DispatchQueue.main.async {
+                let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+                print("Time elapsed for EdgeConnect process: \(timeElapsed) s.")
+
+                completion(image, nil)
+            }
+        }
+    }
+
+    //MARK:- Actions
+
+    @IBAction func importFromLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            self.imagePicker.delegate = self
+            self.imagePicker.sourceType = .photoLibrary
+            self.imagePicker.allowsEditing = false
+            self.present(self.imagePicker, animated: true)
+        } else {
+            print("Photo Library not available")
+        }
+    }
+
+    @IBAction func applyNST() {
+        guard let image = self.imageView.image else {
+            print("Select an image first")
+            return
+        }
+
+        self.isProcessing = true
+        self.process(input: image) { filteredImage, error in
+            self.isProcessing = false
+            if let filteredImage = filteredImage {
+                self.imageView.image = filteredImage
+            } else if let error = error {
+                self.showError(error)
+            } else {
+                self.showError(EdgeConnectError.unknown)
+            }
+        }
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Local variable inserted by Swift 4.2 migrator.
+        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+        if let pickedImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage {
+            self.imageView.image = pickedImage
+            self.imageView.backgroundColor = .clear
+        }
+        self.dismiss(animated: true)
+    }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+    return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+    return input.rawValue
+}
